6183be7476003cfddbf3f0df18d2e38398ce191c
Matt <87963417+MattGet@users.noreply.github.com>
2022-07-22 15:43:56 -0500

    Updated Racks to add 45

---
+++ Fireworks Workshop/Assets/Resources/Joey/JoeyTowerExample.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoeyTowerExample : MonoBehaviour
+{
+    public string TowerName;
+    public GameObject TowerObject;
+    public float AutoAttackDmg = 69;
+    public Texture TowerThumbnail;
+    public Gradient TowerColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
