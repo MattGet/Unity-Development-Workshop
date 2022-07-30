54f5fe5144d99d531658c451c2d1ab4d7727e798
Matt <87963417+MattGet@users.noreply.github.com>
2022-07-30 06:13:58 -0500

    save modding tools

--- Fireworks Workshop/Assets/FM Modding ToolSet by Panini/PreviewGeneration/PrefabToEntityDefenition.cs
+++ Fireworks Workshop/Assets/FM Modding ToolSet by Panini/PreviewGeneration/PrefabToEntityDefenition.cs
@@ -11,7 +11,7 @@ using System.IO;
 
 public class PrefabToEntityDefenition : UnityEditor.Editor
 {
-    [MenuItem("Assets/Prefab To Entity/Default Fireworks")]
+    [MenuItem("Assets/Prefab To Entity/Default Fireworks/Front View")]
     public static void Prefab2Entity()
     {
         GameObject Prefab = Selection.activeGameObject;
@@ -29,7 +29,7 @@ public class PrefabToEntityDefenition : UnityEditor.Editor
         AssetDatabase.SaveAssets();
         EditorUtility.FocusProjectWindow();
 
-        Sprite icon = CaptureImage(Prefab, folderPath);
+        Sprite icon = CaptureImage(Prefab, folderPath, true);
 
         Selection.activeObject = newDef;
         SerializedObject serializedObject = new SerializedObject(newDef);
@@ -91,7 +91,7 @@ public class PrefabToEntityDefenition : UnityEditor.Editor
         EditorUtility.SetDirty(newDef);
     }
 
-    [MenuItem("Assets/Prefab To Entity/Shell")]
+    [MenuItem("Assets/Prefab To Entity/Shell/Front View")]
     public static void Prefab2EntityShell()
     {
         GameObject Prefab = Selection.activeGameObject;
@@ -108,7 +108,7 @@ public class PrefabToEntityDefenition : UnityEditor.Editor
         AssetDatabase.SaveAssets();
         EditorUtility.FocusProjectWindow();
 
-        Sprite icon = CaptureImage(Prefab, folderPath);
+        Sprite icon = CaptureImage(Prefab, folderPath, true);
 
         Selection.activeObject = newDef;
         SerializedObject serializedObject = new SerializedObject(newDef);
@@ -139,7 +139,7 @@ public class PrefabToEntityDefenition : UnityEditor.Editor
         EditorUtility.SetDirty(newDef);
     }
 
-    [MenuItem("Assets/Prefab To Entity/Mortar")]
+    [MenuItem("Assets/Prefab To Entity/Mortar/Front View")]
     public static void Prefab2EntityMortar()
     {
         GameObject Prefab = Selection.activeGameObject;
@@ -156,7 +156,7 @@ public class PrefabToEntityDefenition : UnityEditor.Editor
         AssetDatabase.SaveAssets();
         EditorUtility.FocusProjectWindow();
 
-        Sprite icon = CaptureImage(Prefab, folderPath);
+        Sprite icon = CaptureImage(Prefab, folderPath, true);
 
         Selection.activeObject = newDef;
         SerializedObject serializedObject = new SerializedObject(newDef);
@@ -179,7 +179,7 @@ public class PrefabToEntityDefenition : UnityEditor.Editor
         EditorUtility.SetDirty(newDef);
     }
 
-    [MenuItem("Assets/Prefab To Entity/Prop")]
+    [MenuItem("Assets/Prefab To Entity/Prop/Front View")]
     public static void Prefab2EntityProp()
     {
         GameObject Prefab = Selection.activeGameObject;
@@ -196,7 +196,7 @@ public class PrefabToEntityDefenition : UnityEditor.Editor
         AssetDatabase.SaveAssets();
         EditorUtility.FocusProjectWindow();
 
-        Sprite icon = CaptureImage(Prefab, folderPath);
+        Sprite icon = CaptureImage(Prefab, folderPath, true);
 
         Selection.activeObject = newDef;
         SerializedObject serializedObject = new SerializedObject(newDef);
@@ -219,6 +219,217 @@ public class PrefabToEntityDefenition : UnityEditor.Editor
         EditorUtility.SetDirty(newDef);
     }
 
+#region Back
+
+
+    [MenuItem("Assets/Prefab To Entity/Default Fireworks/Back View")]
+    public static void Prefab2Entity2()
+    {
+        GameObject Prefab = Selection.activeGameObject;
+
+        if (PrefabUtility.GetPrefabAssetType(Prefab) == PrefabAssetType.NotAPrefab || PrefabUtility.GetPrefabAssetType(Prefab) == PrefabAssetType.Model) return;
+
+        FireworkType firework = ReturnType(Prefab);
+        FireworkEntityDefinition newDef = ScriptableObject.CreateInstance<FireworkEntityDefinition>();
+        newDef.name = Prefab.name;
+        string folderPath = AssetDatabase.GetAssetPath(Selection.activeInstanceID);
+        if (folderPath.Contains("."))
+            folderPath = folderPath.Remove(folderPath.LastIndexOf('/'));
+        Debug.Log(folderPath);
+        AssetDatabase.CreateAsset(newDef, $"{ folderPath}/{Prefab.name}.asset");
+        AssetDatabase.SaveAssets();
+        EditorUtility.FocusProjectWindow();
+
+        Sprite icon = CaptureImage(Prefab, folderPath, false);
+
+        Selection.activeObject = newDef;
+        SerializedObject serializedObject = new SerializedObject(newDef);
+
+        serializedObject.FindProperty("_id").stringValue = Prefab.name;
+        serializedObject.FindProperty("_itemName").stringValue = Prefab.name;
+        serializedObject.FindProperty("_prefabGameObject").objectReferenceValue = Prefab;
+        serializedObject.FindProperty("_icon").objectReferenceValue = icon;
+
+        switch (firework)
+        {
+            case FireworkType.Cake:
+                serializedObject.FindProperty("_entityDefinitionType").objectReferenceValue = AssetDatabase.LoadAssetAtPath("Packages/net.laumania.fireworksmania-modtools/FireworksMania/Resources/EntityDefinitionTypes/Fireworks_Cake.asset", typeof(EntityDefinitionType));
+                serializedObject.ApplyModifiedProperties();
+                break;
+            case FireworkType.Tube:
+                serializedObject.FindProperty("_entityDefinitionType").objectReferenceValue = AssetDatabase.LoadAssetAtPath("Packages/net.laumania.fireworksmania-modtools/FireworksMania/Resources/EntityDefinitionTypes/Fireworks_Tube.asset", typeof(EntityDefinitionType));
+                serializedObject.ApplyModifiedProperties();
+                break;
+            case FireworkType.Firecracker:
+                serializedObject.FindProperty("_entityDefinitionType").objectReferenceValue = AssetDatabase.LoadAssetAtPath("Packages/net.laumania.fireworksmania-modtools/FireworksMania/Resources/EntityDefinitionTypes/Fireworks_Firecracker.asset", typeof(EntityDefinitionType));
+                serializedObject.ApplyModifiedProperties();
+                break;
+            case FireworkType.Fountain:
+                serializedObject.FindProperty("_entityDefinitionType").objectReferenceValue = AssetDatabase.LoadAssetAtPath("Packages/net.laumania.fireworksmania-modtools/FireworksMania/Resources/EntityDefinitionTypes/Fireworks_Fountains.asset", typeof(EntityDefinitionType));
+                serializedObject.ApplyModifiedProperties();
+                break;
+            case FireworkType.Rocket:
+                serializedObject.FindProperty("_entityDefinitionType").objectReferenceValue = AssetDatabase.LoadAssetAtPath("Packages/net.laumania.fireworksmania-modtools/FireworksMania/Resources/EntityDefinitionTypes/Fireworks_Rocket.asset", typeof(EntityDefinitionType));
+                serializedObject.ApplyModifiedProperties();
+                break;
+            case FireworkType.Smoke:
+                serializedObject.FindProperty("_entityDefinitionType").objectReferenceValue = AssetDatabase.LoadAssetAtPath("Packages/net.laumania.fireworksmania-modtools/FireworksMania/Resources/EntityDefinitionTypes/Fireworks_Smoke.asset", typeof(EntityDefinitionType));
+                serializedObject.ApplyModifiedProperties();
+                break;
+            case FireworkType.Novelty:
+                serializedObject.FindProperty("_entityDefinitionType").objectReferenceValue = AssetDatabase.LoadAssetAtPath("Packages/net.laumania.fireworksmania-modtools/FireworksMania/Resources/EntityDefinitionTypes/Fireworks_Novelty.asset", typeof(EntityDefinitionType));
+                serializedObject.ApplyModifiedProperties();
+                break;
+            default:
+                Debug.Log("No Suitable Entity Defenition Type Could Be Found!");
+                break;
+        }
+
+        BaseFireworkBehavior Behaviors = Prefab.GetComponent<BaseFireworkBehavior>();
+
+        var prop = Behaviors.GetType().BaseType.GetField("_entityDefinition", System.Reflection.BindingFlags.NonPublic
+     | System.Reflection.BindingFlags.Instance);
+        prop.SetValue(Behaviors, newDef);
+
+
+        SaveableEntity Entity = Prefab.GetComponent<SaveableEntity>();
+        SerializedObject SE = new SerializedObject(Entity);
+        SE.FindProperty("_entityDefinition").objectReferenceValue = newDef;
+
+        SE.ApplyModifiedProperties();
+        serializedObject.ApplyModifiedProperties();
+        EditorUtility.SetDirty(Prefab);
+        EditorUtility.SetDirty(newDef);
+    }
+
+    [MenuItem("Assets/Prefab To Entity/Shell/Back View")]
+    public static void Prefab2EntityShell2()
+    {
+        GameObject Prefab = Selection.activeGameObject;
+
+        if (PrefabUtility.GetPrefabAssetType(Prefab) == PrefabAssetType.NotAPrefab || PrefabUtility.GetPrefabAssetType(Prefab) == PrefabAssetType.Model) return;
+
+        FireworkEntityDefinition newDef = ScriptableObject.CreateInstance<FireworkEntityDefinition>();
+        newDef.name = Prefab.name;
+        string folderPath = AssetDatabase.GetAssetPath(Selection.activeInstanceID);
+        if (folderPath.Contains("."))
+            folderPath = folderPath.Remove(folderPath.LastIndexOf('/'));
+        Debug.Log(folderPath);
+        AssetDatabase.CreateAsset(newDef, $"{ folderPath}/{Prefab.name}.asset");
+        AssetDatabase.SaveAssets();
+        EditorUtility.FocusProjectWindow();
+
+        Sprite icon = CaptureImage(Prefab, folderPath, false);
+
+        Selection.activeObject = newDef;
+        SerializedObject serializedObject = new SerializedObject(newDef);
+
+        serializedObject.FindProperty("_id").stringValue = Prefab.name;
+        serializedObject.FindProperty("_itemName").stringValue = Prefab.name;
+        serializedObject.FindProperty("_prefabGameObject").objectReferenceValue = Prefab;
+        serializedObject.FindProperty("_icon").objectReferenceValue = icon;
+
+        serializedObject.FindProperty("_entityDefinitionType").objectReferenceValue = AssetDatabase.LoadAssetAtPath("Packages/net.laumania.fireworksmania-modtools/FireworksMania/Resources/EntityDefinitionTypes/Fireworks_Shell.asset", typeof(EntityDefinitionType));
+        serializedObject.ApplyModifiedProperties();
+
+
+        BaseFireworkBehavior Behaviors = Prefab.GetComponent<BaseFireworkBehavior>();
+
+        var prop = Behaviors.GetType().BaseType.GetField("_entityDefinition", System.Reflection.BindingFlags.NonPublic
+     | System.Reflection.BindingFlags.Instance);
+        prop.SetValue(Behaviors, newDef);
+
+
+        SaveableEntity Entity = Prefab.GetComponent<SaveableEntity>();
+        SerializedObject SE = new SerializedObject(Entity);
+        SE.FindProperty("_entityDefinition").objectReferenceValue = newDef;
+
+        SE.ApplyModifiedProperties();
+        serializedObject.ApplyModifiedProperties();
+        EditorUtility.SetDirty(Prefab);
+        EditorUtility.SetDirty(newDef);
+    }
+
+    [MenuItem("Assets/Prefab To Entity/Mortar/Back View")]
+    public static void Prefab2EntityMortar2()
+    {
+        GameObject Prefab = Selection.activeGameObject;
+
+        if (PrefabUtility.GetPrefabAssetType(Prefab) == PrefabAssetType.NotAPrefab || PrefabUtility.GetPrefabAssetType(Prefab) == PrefabAssetType.Model) return;
+
+        PropEntityDefinition newDef = ScriptableObject.CreateInstance<PropEntityDefinition>();
+        newDef.name = Prefab.name;
+        string folderPath = AssetDatabase.GetAssetPath(Selection.activeInstanceID);
+        if (folderPath.Contains("."))
+            folderPath = folderPath.Remove(folderPath.LastIndexOf('/'));
+        Debug.Log(folderPath);
+        AssetDatabase.CreateAsset(newDef, $"{ folderPath}/{Prefab.name}.asset");
+        AssetDatabase.SaveAssets();
+        EditorUtility.FocusProjectWindow();
+
+        Sprite icon = CaptureImage(Prefab, folderPath, false);
+
+        Selection.activeObject = newDef;
+        SerializedObject serializedObject = new SerializedObject(newDef);
+
+        serializedObject.FindProperty("_id").stringValue = Prefab.name;
+        serializedObject.FindProperty("_itemName").stringValue = Prefab.name;
+        serializedObject.FindProperty("_prefabGameObject").objectReferenceValue = Prefab;
+        serializedObject.FindProperty("_icon").objectReferenceValue = icon;
+
+        serializedObject.FindProperty("_entityDefinitionType").objectReferenceValue = AssetDatabase.LoadAssetAtPath("Packages/net.laumania.fireworksmania-modtools/FireworksMania/Resources/EntityDefinitionTypes/Fireworks_Mortar.asset", typeof(EntityDefinitionType));
+        serializedObject.ApplyModifiedProperties();
+
+        SaveableEntity Entity = Prefab.GetComponent<SaveableEntity>();
+        SerializedObject SE = new SerializedObject(Entity);
+        SE.FindProperty("_entityDefinition").objectReferenceValue = newDef;
+
+        SE.ApplyModifiedProperties();
+        serializedObject.ApplyModifiedProperties();
+        EditorUtility.SetDirty(Prefab);
+        EditorUtility.SetDirty(newDef);
+    }
+
+    [MenuItem("Assets/Prefab To Entity/Prop/Back View")]
+    public static void Prefab2EntityProp2()
+    {
+        GameObject Prefab = Selection.activeGameObject;
+
+        if (PrefabUtility.GetPrefabAssetType(Prefab) == PrefabAssetType.NotAPrefab || PrefabUtility.GetPrefabAssetType(Prefab) == PrefabAssetType.Model) return;
+
+        PropEntityDefinition newDef = ScriptableObject.CreateInstance<PropEntityDefinition>();
+        newDef.name = Prefab.name;
+        string folderPath = AssetDatabase.GetAssetPath(Selection.activeInstanceID);
+        if (folderPath.Contains("."))
+            folderPath = folderPath.Remove(folderPath.LastIndexOf('/'));
+        Debug.Log(folderPath);
+        AssetDatabase.CreateAsset(newDef, $"{ folderPath}/{Prefab.name}.asset");
+        AssetDatabase.SaveAssets();
+        EditorUtility.FocusProjectWindow();
+
+        Sprite icon = CaptureImage(Prefab, folderPath, false); ;
+
+        Selection.activeObject = newDef;
+        SerializedObject serializedObject = new SerializedObject(newDef);
+
+        serializedObject.FindProperty("_id").stringValue = Prefab.name;
+        serializedObject.FindProperty("_itemName").stringValue = Prefab.name;
+        serializedObject.FindProperty("_prefabGameObject").objectReferenceValue = Prefab;
+        serializedObject.FindProperty("_icon").objectReferenceValue = icon;
+
+        serializedObject.FindProperty("_entityDefinitionType").objectReferenceValue = AssetDatabase.LoadAssetAtPath("Packages/net.laumania.fireworksmania-modtools/FireworksMania/Resources/EntityDefinitionTypes/Prop.asset", typeof(EntityDefinitionType));
+        serializedObject.ApplyModifiedProperties();
+
+        SaveableEntity Entity = Prefab.GetComponent<SaveableEntity>();
+        SerializedObject SE = new SerializedObject(Entity);
+        SE.FindProperty("_entityDefinition").objectReferenceValue = newDef;
+
+        SE.ApplyModifiedProperties();
+        serializedObject.ApplyModifiedProperties();
+        EditorUtility.SetDirty(Prefab);
+        EditorUtility.SetDirty(newDef);
+    }
+    #endregion
 
     private static FireworkType ReturnType(GameObject item)
     {
@@ -288,13 +499,21 @@ public class PrefabToEntityDefenition : UnityEditor.Editor
         Null,
     }
 
-    public static Sprite CaptureImage(GameObject pref, string path)
+    public static Sprite CaptureImage(GameObject pref, string path, bool front)
     {
         int width = 512;
         int height = 512;
         RuntimePreviewGenerator.BackgroundColor = Color.clear;
         RuntimePreviewGenerator.MarkTextureNonReadable = false;
-        RuntimePreviewGenerator.PreviewDirection = new Vector3(-0.75f, -1, -1.5f);
+        if (front)
+        {
+            RuntimePreviewGenerator.PreviewDirection = new Vector3(-0.75f, -1, 1.5f);
+        }
+        else
+        {
+            RuntimePreviewGenerator.PreviewDirection = new Vector3(-0.75f, -1, -1.5f);
+        }
+
         RuntimePreviewGenerator.RenderSupersampling = 2;
         RuntimePreviewGenerator.OrthographicMode = true;
 
