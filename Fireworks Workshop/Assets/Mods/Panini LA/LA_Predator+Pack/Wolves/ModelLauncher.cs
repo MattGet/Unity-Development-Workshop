05fb09afb3f5419a4da547c30a959225d3df45f1
Matt Dembny <matthewdembny@gmail.com>
2024-11-16 17:59:31 -0500

    Update for MP and script fix

--- Fireworks Workshop/Assets/Mods/Panini LA/LA_Predator+Pack/Wolves/ModelLauncher.cs
+++ Fireworks Workshop/Assets/Mods/Panini LA/LA_Predator+Pack/Wolves/ModelLauncher.cs
@@ -5,6 +5,7 @@ using FireworksMania.Core.Behaviors.Fireworks.Parts;
 using FireworksMania.Core.Behaviors.Fireworks;
 using FireworksMania.Core.Messaging;
 using FireworksMania.Core.Definitions;
+using Unity.Netcode;
 
 public class ModelLauncher : MonoBehaviour
 {
@@ -22,6 +23,7 @@ public class ModelLauncher : MonoBehaviour
     private bool onlydoonce;
     private Rigidbody _rigidbody;
     public Fuse CakeFuse;
+    public GameObject topLevelObject;
     private GameObject scene;
     public GameSoundDefinition Sound;
     public ParticleSystem LaunchEffect;
@@ -29,7 +31,7 @@ public class ModelLauncher : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        scene = GameObject.Find("[FireworksManager]");
+        scene = GameObject.Find("FireworksManager");
     }
 
     // Update is called once per frame
@@ -46,21 +48,26 @@ public class ModelLauncher : MonoBehaviour
     {
         yield return new WaitForSeconds(StartDelay);
         float startpos = (((Number / 2) - 0.5f) * spacing) * (-1f);
+
+        List<GameObject> spawnObjects = new List<GameObject>();
         for (int i = 1; i <= Number; i++)
         {
             float up = Random.Range(StartSpeedyMIN, StartSpeedyMAX);
             float variance1 = Random.Range(StartSpeedvarMIN, StartSpeedvarMAX);
             float variance2 = Random.Range(StartSpeedvarMIN, StartSpeedvarMAX);
             GameObject item = Instantiate(Model, this.gameObject.transform);
+            spawnObjects.Add(item);
+            var networkObject = item.GetComponent<NetworkObject>();
+            networkObject.Spawn();
             if (LaunchEffect != null)
             {
                 GameObject effect = Instantiate(LaunchEffect.gameObject, this.gameObject.transform);
+                spawnObjects.Add(effect);
                 effect.transform.localPosition = new Vector3(0, 0, startpos);
                 effect.transform.parent = scene.transform;
                 effect.transform.localScale = new Vector3(1, 1, 1);
 
             }
-            item.transform.localPosition = new Vector3(0, 0, startpos);
 
             _rigidbody = item.GetComponent<Rigidbody>();
 
@@ -74,13 +81,24 @@ public class ModelLauncher : MonoBehaviour
             {
                 Messenger.Broadcast(new MessengerEventPlaySound(Sound.name, gameObject.transform, true, false));
             }
+
+            yield return new WaitForSeconds(0.01f);
+
             item.transform.parent = scene.transform;
+            item.transform.position = this.transform.position;
+            item.transform.rotation = this.transform.rotation;
             item.transform.localScale = new Vector3(1, 1, 1);
             _rigidbody.AddRelativeForce(new Vector3(variance1, up, variance2), ForceMode.Impulse);
 
 
             startpos = startpos + spacing;
             yield return new WaitForSeconds(TimeDelayLaunch);
+        }
+
+        yield return new WaitForSeconds(45f);
+
+        foreach (GameObject item in spawnObjects){
+            NetworkObject.Destroy(item);
         }
     }
 }
